c458e8b2ab82390c8bdbd43874714f616e531cf6
Christopher Marchant <cmarch240@gmail.com>
2022-02-03 15:33:08 -0600

    Made certain Hangman methods private

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -8,25 +8,6 @@ class Hangman
     self.guessed_letters = []
   end
 
-  def evaluate_guess(guess)
-    if self.secret_word.include?(guess) && !self.correct_letters.include?(guess)
-      self.correct_letters << guess
-    elsif self.correct_letters.include?(guess)
-      decrement_turns_remaining
-    else
-      decrement_turns_remaining
-    end
-    guessed_letters << guess
-  end
-
-  def display_word
-    letter_array = Array.new(self.secret_word.length, '___')
-    self.secret_word.each_char.with_index do |letter, index| 
-      letter_array[index] = letter if self.correct_letters.include?(letter)
-    end
-    letter_array.join(" ")
-  end
-
   def game_over?
     correct_guess? || self.turns_remaining == 0
   end
@@ -48,18 +29,35 @@ class Hangman
 
   private
 
+  def choose_secret_word
+    File.readlines('acceptable_hangman_words.txt').sample.strip
+  end
+
   def decrement_turns_remaining
     self.turns_remaining -= 1
   end
 
-  def choose_secret_word
-    File.readlines('acceptable_hangman_words.txt').sample.strip
+  def evaluate_guess(guess)
+    if self.secret_word.include?(guess) && !self.correct_letters.include?(guess)
+      self.correct_letters << guess
+    elsif self.correct_letters.include?(guess)
+      decrement_turns_remaining
+    else
+      decrement_turns_remaining
+    end
+    guessed_letters << guess
+  end
+
+  def display_word
+    letter_array = Array.new(self.secret_word.length, '___')
+    self.secret_word.each_char.with_index do |letter, index| 
+      letter_array[index] = letter if self.correct_letters.include?(letter)
+    end
+    letter_array.join(" ")
   end
 
   def correct_guess?
     self.secret_word.split("").all? { |letter| self.correct_letters.include?(letter) }
   end
 
-
 end
-
