3bb77e8a91928c2e0a172e7e1f1fe466b456d9cb
Christopher Marchant <cmarch240@gmail.com>
2022-02-03 14:07:58 -0600

    Created Hangman.evaluate_guess

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -8,6 +8,12 @@ class Hangman
     self.incorrect_letters = []
   end
 
+  def evaluate_guess(secret_word, guess)
+    if secret_word.include?(guess) && !self.correct_letters.include?(guess)
+      self.correct_letters << guess
+    end
+  end
+
   def display_word(secret_word_string, correct_letters_array)
     letter_array = Array.new(secret_word_string.length, '___')
     secret_word_string.each_char.with_index do |letter, index| 
@@ -22,7 +28,4 @@ class Hangman
   end
 
 end
-
-game = Hangman.new
-p game.display_word("christopher", ['c', 'h', 'r', 'p'])
 
