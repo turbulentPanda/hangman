a163ebead30209b9adb1e155d9bd5f3f71d376a1
Christopher Marchant <cmarch240@gmail.com>
2022-02-03 12:49:48 -0600

    Created Hangman class and defined initialize constructor method

---
+++ lib/hangman.rb
@@ -0,0 +1,10 @@
+class Hangman
+  attr_accessor :secret_word, :turns_remaining, :correct_letters, :incorrect_letters
+
+  def initialize
+    self.turns_remaining = 6
+    self.correct_letters = []
+    self.incorrect_letters = []
+  end
+
+end
